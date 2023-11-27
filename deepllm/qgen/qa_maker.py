05f14ae5ba27a773e6756e3ddbc10fcae394f81c
Paul Tarau <ptarau@gmail.com>
2023-11-27 14:34:27 -0600

    progress on generating QA+follow-up Q threads

--- deepllm/qgen/qa_maker.py
+++ deepllm/qgen/qa_maker.py
@@ -13,10 +13,12 @@ def clean_sent(sent):
 
 
 def clean_quest(x0, sent, context):
-    # print('!!! CLEANING:',x)
     x = x0.strip()
+    #print('!!! CLEANING:', x)
 
     assert x, ("Empty!!!!", (sent, context))
+
+    assert 'A:' in x or 'Q:' in x, ('MISSING A: or Q:', x)
 
     if x[0] in ("'", '"'): x = x[1:]
     if x[-1] in ("'", '"'): x = x[0:-1]
@@ -25,7 +27,7 @@ def clean_quest(x0, sent, context):
     return x
 
 
-def to_quests(agent, question, context, short_size=4, k=3):
+def to_quests(agent, question, context, k=3):
     agent.set_pattern(None)
     p = f"""
     With the context "{context}" in mind,
@@ -38,7 +40,7 @@ def to_quests(agent, question, context, short_size=4, k=3):
     answer = agent.ask(prompt)
 
     # print('PROMPT:',prompt)
-    # print('RETURN:',answer)
+    #print('!!! RETURN:\n',answer)
     # print()
 
     return answer
@@ -51,12 +53,13 @@ def quest2quests(agent, quest, context, k=3):
     quests0 = quests_.replace('\n\n', '\n').split('\n')
     quests = [clean_quest(q, quest, context) for q in quests0]
     # print('LENS:', len(quests0), len(quests))
-    assert len(quests) % 2 == 0, (len(quests0), len(quests), quests0)
+    if len(quests)% 2 !=0:
+        quests=quests[0:-1] # fix LLM ignoring instruction
+    #assert len(quests) % 2 == 0, (len(quests0), len(quests), quest, quests0)
 
     pairs = []
     for j, x in enumerate(quests):
 
-        # print('    ' + x)
         p = x[0:3]
         assert p in ['Q: ', 'A: ']
         x = x[3:]
@@ -66,7 +69,7 @@ def quest2quests(agent, quest, context, k=3):
 
             q = quests[j + 1]
             p_ = q[0:3]
-            q = q[3:]  # quest: previous position
+            q = q[3:]  # quest: next position
             assert p_ == "Q: ", (p_, q)
             pair = (a, q)
             pairs.append(pair)
@@ -102,23 +105,37 @@ def localize(local):
         cheaper_model()
 
 
-def recursor(initiator, trim_size=4, max_k=2, max_d=5):
+def recursor(initiator, trim_size=3, max_k=2, max_d=5):
     agent = make_agent()
+    seen = {initiator}
 
     def generate(quest, d):
+
+        def thread_end(a, q):
+            if a in seen or q in seen:
+                return ()
+            elif d >= max_d:
+                return (quest, a),
+            else:
+                seen.add(a)
+                seen.add(q)
+                return None
+
         pairs = quest2quests(agent, quest, initiator, k=max_k)
         agent.trim_at(trim_size)
         for a, q in pairs:
-            if d >= max_d:
-                yield (quest,a),
+            end = thread_end(a, q)
+            if end is not None:
+                yield end
             else:
                 for trace in generate(q, d + 1):
                     yield ((quest, a),) + trace
 
     for trace in generate(initiator, 0):
-        agent.persist()
-        show_mems(agent)
-        yield trace
+        if trace:
+            show_mems(agent)
+            agent.persist()
+            yield trace
 
 
 def show_mems(agent):
@@ -128,11 +145,13 @@ def show_mems(agent):
           len(agent.long_mem),
           'COSTS:', round(agent.dollar_cost(), 4))
 
+
 def test_qa_maker(fresh=0, local=1):
     localize(local)
     agent = make_agent()
     agent.resume()
-    initiator = "Why do some people think that we live in a simulation?"
+    #initiator = "Why do some people think that we live in a simulation?"
+    initiator = "How do transformers work?"
     print('INITIATOR:', initiator)
     for thread in recursor(initiator):
         print('\nTHREAD:\n')
@@ -141,7 +160,6 @@ def test_qa_maker(fresh=0, local=1):
             print('A:', a)
             print()
         print()
-        agent.persist()
     if fresh: agent.clear()
     print('SHORT_TERM_MEMORY SIZE:',
           len(agent.short_mem),
