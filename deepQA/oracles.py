f18552e317729d2b44f8a87e3f2bd511e5599127
Paul Tarau <ptarau@gmail.com>
2024-01-01 22:10:49 -0600

    first cut at oracles in deepQA

--- deepQA/oracles.py
+++ deepQA/oracles.py
@@ -1,20 +1,29 @@
 from deepllm.api import *
+from sentify.main import sentify
 from deepllm.embedders import Embedder
-from deepllm.refiners import load_ground_truth
-from inquisitor import QuestExplorer
+from inquisitor import QuestExplorer,quest_prompter
 
 
 class TruthJudge(QuestExplorer):
-    def __init__(self, initiator=None, prompter=None, truth_file=None, top_k=None, threshold=None, lim=None):
-        assert None not in (initiator, prompter, truth_file, top_k, threshold, lim)
-        super.__init__(initiator=initiator, prompter=prompter, lim=lim)
+    def __init__(self,
+                 initiator=None,
+                 prompter=None,
+                 file_type=None,
+                 truth_file=None,
+                 top_k=None,
+                 threshold=None,
+                 lim=None,
+                 local=None
+                 ):
+        assert None not in (initiator, prompter, file_type, truth_file, top_k, threshold, lim, local)
+        super().__init__(initiator=initiator, prompter=prompter, lim=lim, local=local)
 
         self.top_k = top_k
         self.threshold = threshold
         self.store = Embedder(truth_file)
         self.truth_file = truth_file
         if not exists_file(self.store.cache()):
-            sents = load_ground_truth(truth_file=truth_file)
+            sents = sentify(file_type, truth_file)
             self.store.store(sents)
 
         def appraise(self, g, _trace):
@@ -31,3 +40,20 @@ class TruthJudge(QuestExplorer):
             tprint('AS AVG. OF NEAREST SENTS:')
             for sent, r in sents_rs: tprint(sent, '->', round(r, 4))
             return ok
+
+def test_oracles():
+    tg=TruthJudge(
+        initiator="What is SLD resolution?",
+        prompter=quest_prompter,
+        file_type='wikipage',
+        truth_file='Logic Programming',
+        top_k=3,
+        threshold=0.50,
+        lim=2,
+        local=0
+    )
+    tg.run()
+
+
+if __name__=="__main__":
+    test_oracles()
